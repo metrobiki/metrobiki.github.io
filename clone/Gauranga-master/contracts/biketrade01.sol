f74f0bef9b6d72c79e6f730dea000ecefb26b7e2
Harshit <doreplymail@gmail.com>
2019-03-21 18:02:00 +0530

    initial commit

---
+++ clone/Gauranga-master/contracts/biketrade01.sol
@@ -0,0 +1,25 @@
+pragma solidity ^0.5.0;
+
+import "./metrobikitrade.sol";               
+
+contract ForSale is Review {
+
+    //event NewBicycleForSale(_bicycleId, Model, Color, Price);
+
+    uint[] public bicycleForSale;
+
+    function putForSale(uint _bicycleId, uint _price) public {
+        require(msg.sender == bicycleToOwner[_bicycleId], "Not owner of bicycle!");
+        Bicycle storage myBicycle = bicycles[_bicycleId];
+        myBicycle.state = State.FORSALE;
+        myBicycle.price = _price;
+        bicycleForSale.push(_bicycleId);
+        //emit NewBicycleForSale(myBicycle.Model, myBicycle.Color, myBicycle.price);
+    }
+
+    function displayBicyclesForSale() external view returns(uint[] memory) {
+        return bicycleForSale;
+    }
+
+
+}
